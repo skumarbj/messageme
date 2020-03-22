45c5a111fdf338dd14b25186c163e6acdd488b03
Senthilkumar <senthilkumar.b3@happiestminds.com>
2020-03-22 08:59:32 +0530

    User resource added with test/sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Shiva", password: "password")
+User.create(username: "Senthil", password: "password")
+User.create(username: "Evgeny", password: "password")
+User.create(username: "Jonsnow", password: "password")
+User.create(username: "Arya", password: "password")
+User.create(username: "Frodo", password: "password")
+User.create(username: "Gandalf", password: "password")
